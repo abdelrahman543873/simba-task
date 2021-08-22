157c6a0235bd4c54a0139e9641b6cbc37ae17e30
abdelrahman543873 <abdelrahmanmysara33@gmail.com>
2021-08-22 04:55:16 +0200

    test refactor

--- test/user/register.spec.ts
+++ test/user/register.spec.ts
@@ -5,7 +5,7 @@ import { buildUserParams } from '../../src/user/user.factory';
 import { GET_MY_TRANSACTIONS } from '../endpoints/transaction';
 import { User } from '../../src/user/models/user.model';
 describe('user register suite case', () => {
-  it('register user and should have a transaction of 1000 usd', async () => {
+  it('register user and should have a transaction of 1000 usd and throw an error if user register with the same mail', async () => {
     const user = await buildUserParams();
     const res = await testRequest({
       method: HTTP_METHODS_ENUM.POST,
@@ -20,22 +20,11 @@ describe('user register suite case', () => {
       token: res.body.token,
     });
     expect(res1.body.length).toBe(1);
-  });
-
-  it('should throw error if a user exists with the same email', async () => {
-    const user = await buildUserParams();
-    const res = await testRequest({
+    const res2 = await testRequest({
       method: HTTP_METHODS_ENUM.POST,
       url: REGISTER,
       variables: user,
     });
-    expect(res.body.name).toBe(user.name);
-    expect(res.body.address).toBe(user.address);
-    const res1 = await testRequest({
-      method: HTTP_METHODS_ENUM.POST,
-      url: REGISTER,
-      variables: user,
-    });
-    expect(res1.body.statusCode).toBe(607);
+    expect(res2.body.statusCode).toBe(607);
   });
 });
